dca0bcce389f873ca24c489c907b81938aaf49fa
lincolncodelinc <79852648+lincolncodelinc@users.noreply.github.com>
2021-03-04 14:23:41 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/hello-world'),
+      dir: require('path').join(__dirname, './coverage/servant-center'),
       subdir: '.',
       reporters: [
         { type: 'html' },
