39af48ecc84328a61f3e9fd6d59e8303f6af72eb
lincolncodelinc <79852648+lincolncodelinc@users.noreply.github.com>
2021-03-04 14:26:09 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hello-world'`, () => {
+  it(`should have as title 'servant-center'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-world');
+    expect(app.title).toEqual('servant-center');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hello-world app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('servant-center app is running!');
   });
 });
