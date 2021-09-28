0e5027e337fd6b928fa4be00d16063345494dd9c
Akash kumar <akash.kumar6@mindtree.com>
2021-09-28 11:42:25 +0530

    consent model initial version

---
+++ src/app/shared/components/consent-data/consent-data.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConsentDataComponent } from './consent-data.component';
+
+describe('ConsentDataComponent', () => {
+  let component: ConsentDataComponent;
+  let fixture: ComponentFixture<ConsentDataComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConsentDataComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConsentDataComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -28,6 +28,9 @@ import { ToggleButtonModule } from 'primeng/togglebutton';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { KeysPipe } from '../case-worker/components/resident-search/rs-initial-assessment/ia-form-page-three/ia-form-page-three.component';
+import { ConsentDataComponent } from './components/consent-data/consent-data.component';
+import {DialogModule} from 'primeng/dialog';
+import {HttpClientModule} from '@angular/common/http'
 
 
 FullCalendarModule.registerPlugins([ // register FullCalendar plugins
@@ -56,7 +59,9 @@ const MODULES = [
     CheckboxModule,
     InputSwitchModule,
     ToolbarModule,
-    ToggleButtonModule
+    ToggleButtonModule,
+    DialogModule,
+    HttpClientModule
 ];
 
 const COMPONENTS = [
@@ -68,7 +73,8 @@ const COMPONENTS = [
 
 @NgModule({
   declarations: [
-    ...COMPONENTS
+    ...COMPONENTS,
+    ConsentDataComponent
   ],
   imports: [
     CommonModule,
