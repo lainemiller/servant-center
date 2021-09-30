e8f45636cd8e19b38ac82d2af584ea8f89a9e394
Sam Kamalesan <sam.kamalesan@lfg.com>
2021-09-30 13:10:28 +0530

    shared module formatted

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,40 +1,40 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { CalendarModule } from 'primeng/calendar';
-import { DropdownModule } from 'primeng/dropdown';
-import { DividerModule } from "primeng/divider";
-import { MenuModule } from 'primeng/menu';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from '@fullcalendar/list';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin  from "@fullcalendar/interaction";
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ItemHeaderComponent } from './components/item-header/item-header.component';
-import { TableModule } from 'primeng/table';
-import { TabViewModule } from "primeng/tabview";
-import { StepsModule } from 'primeng/steps'
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
 import { CardModule } from "primeng/card";
-import { TabMenuModule } from "primeng/tabmenu";
-import {InputTextareaModule} from 'primeng/inputtextarea';
-import { RadioButtonModule } from "primeng/radiobutton";
 import { CheckboxModule } from 'primeng/checkbox';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from "primeng/divider";
+import { DropdownModule } from 'primeng/dropdown';
 import { InputSwitchModule } from "primeng/inputswitch";
-import { ToolbarModule } from "primeng/toolbar";
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { MenuModule } from 'primeng/menu';
+import { RadioButtonModule } from "primeng/radiobutton";
+import { StepsModule } from 'primeng/steps';
+import { TableModule } from 'primeng/table';
+import { TabMenuModule } from "primeng/tabmenu";
+import { TabViewModule } from "primeng/tabview";
 import { ToggleButtonModule } from 'primeng/togglebutton';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { ToolbarModule } from "primeng/toolbar";
+import { TooltipModule } from 'primeng/tooltip';
+
 import { KeysPipe } from '../case-worker/components/resident-search/rs-initial-assessment/ia-form-page-three/ia-form-page-three.component';
 import { ConsentDataComponent } from './components/consent-data/consent-data.component';
-import {HttpClientModule} from '@angular/common/http'
-import {DialogModule} from 'primeng/dialog';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import {Tooltip, TooltipModule} from 'primeng/tooltip';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ItemHeaderComponent } from './components/item-header/item-header.component';
 import { WelcomeHeaderComponent } from './components/welcome-header/welcome-header.component';
-
 
 FullCalendarModule.registerPlugins([ // register FullCalendar plugins
   dayGridPlugin,
@@ -43,31 +43,30 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
   interactionPlugin
 ]);
 const MODULES = [
-    InputTextModule,
-    ButtonModule,
-    CalendarModule,
-    DividerModule,
-    MenuModule,
-    DropdownModule,
-    FullCalendarModule,
-    FormsModule,
-    ReactiveFormsModule,
-    TableModule,
-    TabViewModule,
-    TabMenuModule,
-    StepsModule,
-    CardModule,
-    InputTextareaModule,
-    RadioButtonModule,
-    CheckboxModule,
-    InputSwitchModule,
-    ToolbarModule,
-    ToggleButtonModule,
-    TooltipModule,
-    DialogModule,
-    Ng2SearchPipeModule,
-    HttpClientModule
-
+  InputTextModule,
+  ButtonModule,
+  CalendarModule,
+  DividerModule,
+  MenuModule,
+  DropdownModule,
+  FullCalendarModule,
+  FormsModule,
+  ReactiveFormsModule,
+  TableModule,
+  TabViewModule,
+  TabMenuModule,
+  StepsModule,
+  CardModule,
+  InputTextareaModule,
+  RadioButtonModule,
+  CheckboxModule,
+  InputSwitchModule,
+  ToolbarModule,
+  ToggleButtonModule,
+  TooltipModule,
+  DialogModule,
+  Ng2SearchPipeModule,
+  HttpClientModule
 ];
 
 const COMPONENTS = [
