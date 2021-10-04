c3b16b0a039a9d4f582cadffef1a8c440306794a
apeksha3896 <Apeksha.Horambe@mindtree.com>
2021-10-04 18:01:10 +0530

    commited changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ButtonModule } from 'primeng/button';
+
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { ButtonModule } from 'primeng/button';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
