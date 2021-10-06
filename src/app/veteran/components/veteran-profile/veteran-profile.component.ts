25c42cb0e0c09142594092c629826e0585b3a4f7
KottoorNavya <kottoor.navya@mindtree.com>
2021-10-06 21:30:23 +0530

    Veteran profile - Latest

--- src/app/veteran/components/veteran-profile/veteran-profile.component.ts
+++ src/app/veteran/components/veteran-profile/veteran-profile.component.ts
@@ -24,17 +24,27 @@ export class VeteranProfileComponent implements OnInit {
   @email()
   emailId!: string;
 
-  genders: any[] = [{ name: 'Female', key: 'A' }, { name: 'Male', key: 'M' }];
-  martialStatuses: any[] = [{ name: 'Single', key: 'S' }, { name: 'Married', key: 'M' }, { name: 'DIvorced', key: 'D' }, { name: 'Widowed', key: 'W' }];
+  genders: any[] = [
+    { name: 'Female', key: 'A' },
+    { name: 'Male', key: 'M' },
+  ];
+  martialStatuses: any[] = [
+    { name: 'Single', key: 'S' },
+    { name: 'Married', key: 'M' },
+    { name: 'DIvorced', key: 'D' },
+    { name: 'Widowed', key: 'W' },
+  ];
 
-  constructor(private formBuilder: FormBuilder, private service: VeteranprofileService) {
-
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: VeteranprofileService
+  ) {
     this.states = [
       { name: 'New York' },
       { name: 'Rome' },
       { name: 'London' },
       { name: 'Istanbul' },
-      { name: 'Paris' }
+      { name: 'Paris' },
     ];
   }
 
@@ -44,7 +54,7 @@ export class VeteranProfileComponent implements OnInit {
     this.selectedGender = this.genders[1];
     this.selectedMartialStatus = this.martialStatuses[1];
     let response = this.service.getVeteranProfileDetailsByRecordNumber();
-    response.subscribe((data) => this.veteran = data)
+    response.subscribe((data) => (this.veteran = data));
   }
 
   buildForm() {
@@ -73,7 +83,11 @@ export class VeteranProfileComponent implements OnInit {
       POB: ['', Validators.required],
       SSNNumber: ['', RxwebValidators.mask({ mask: '999-99-9999' })],
       hmisIdNo: ['', Validators.required],
-      emailId: [null,Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')],
+      emailId: [
+        null,
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+      ],
       veteranSupports: ['', Validators.required],
       phoneNumber: ['', RxwebValidators.mask({ mask: '(999) 999-9999' })],
       primaryLanguage: ['', Validators.required],
@@ -100,7 +114,3 @@ export class VeteranProfileComponent implements OnInit {
     console.log(this.veteranProfileForm.value);
   }
 }
-
-
-
-
