fa81a442b9b39f43a8f74823f5566d50d83d0a90
Pravin Bhilare <pravin.bhilare@mindtree.com>
2021-10-12 10:39:19 +0530

    case-worker

--- src/app/case-worker/case-worker.component.ts
+++ src/app/case-worker/case-worker.component.ts
@@ -9,7 +9,7 @@ import { DataService } from './services/data.service';
 })
 export class CaseWorkerComponent implements OnInit {
   displayMenu: boolean = true;
-  public msgCount!: number;
+  public msgCount: number=1;
   public msgData: any;
   public userInfo: any;
   public name!: string;
@@ -21,19 +21,26 @@ export class CaseWorkerComponent implements OnInit {
     this.displayMenu = window.innerWidth > 768;
   }
   constructor(private service: DataService) {
-    this.service.getMsgCount().subscribe((data) => {
+    this.service.getMsgCount().subscribe((data:any)=>
+    {
       this.msgData = data;
       this.msgCount = this.msgData.msgs.length;
-      this.items[1].label = `MESSAGES (${this.msgCount})+`;
-    });
-
+      this.itemChange(this.msgCount);
+    },
+    (err:any)=>
+    {
+      this.itemChange(0);
+    })
     this.service.getName().subscribe((data) => {
       this.userInfo = data;
       this.profilePic = this.userInfo.image;
       this.name = this.userInfo.name;
       this.login = this.userInfo.date;
     });
-
+  
+  }
+  itemChange(msgs:number)
+  {
     this.items = [
       {
         label: 'DASHBOARD',
@@ -41,28 +48,24 @@ export class CaseWorkerComponent implements OnInit {
         styleClass: 'menu-items--text menu-item--1',
         routerLink: ['/case-worker/'],
       },
-
       {
-        label: `MESSAGES`,
+        label: `MESSAGES (${msgs})+`,
         icon: 'fa fa-bell',
         styleClass: 'menu-items--text menu-item--2',
         routerLink: ['/case-worker/messages'],
       },
-
       {
         label: 'RESIDENT SEARCH',
         icon: 'fa fa-search',
         styleClass: 'menu-items--text menu-item--3',
         routerLink: ['/case-worker/resident-search'],
       },
-
       {
         label: 'PROFILE',
         icon: 'fa fa-user-circle',
         styleClass: 'menu-items--text menu-item--5',
         routerLink: ['/case-worker/profile'],
       },
-
       {
         label: 'LOGOUT',
         icon: 'fas fa-sign-out-alt',
