35b47aa877692502e43927a32a994c145b2f546c
Pravin Bhilare <pravin.bhilare@mindtree.com>
2021-11-11 12:20:34 +0530

    login working

--- src/app/shared/components/login-page/login-page.component.ts
+++ src/app/shared/components/login-page/login-page.component.ts
@@ -1,10 +1,6 @@
-import { Component, NgZone, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment';
 import { Auth } from '@aws-amplify/auth';
-import { Hub } from '@aws-amplify/core';
-import { AmplifyService } from 'aws-amplify-angular';
 
 @Component({
   selector: 'app-login-page',
@@ -13,80 +9,20 @@ import { AmplifyService } from 'aws-amplify-angular';
 })
 export class LoginPageComponent implements OnInit {
   public title = 'LOGIN TO APPLICATION';
-  public logInUrl =
-    'https://servant-center.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=5igt34gtp2ok5mf02d9e4v30lb&redirect_uri=http://localhost:4200/veteran';
-  public loginForm: any;
-
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private amplifyService: AmplifyService,
-    private zone: NgZone
-  ) {
-    console.log(Hub.listen('auth', ({ payload: { event, data } }) => {
-      console.log(event);
-      console.log(data);
-      
-      
-      if (event === 'cognitoHostedUI' || event === 'signedIn') {
-        console.log(event,"login success");
-        this.zone.run(() => this.router.navigate(['/veteran']));
-      } else {
-        console.log('hgjh');
-      }
-    }));
-    
-    Hub.listen('auth', ({ payload: { event, data } }) => {
-      console.log(event);
-      console.log(data);
-      
-      
-      if (event === 'cognitoHostedUI' || event === 'signedIn') {
-        console.log(event,"login success");
-        this.zone.run(() => this.router.navigate(['/veteran']));
-      } else {
-        console.log('hgjh');
-      }
-    });
-
+  constructor(private router: Router) {
     //currentAuthenticatedUser: when user comes to login page again
-    console.log(Auth.currentAuthenticatedUser());
-    
     Auth.currentAuthenticatedUser()
-      .then((user) => {
-        console.log("login again ");
-        
+      .then(() => {
         this.router.navigate(['/veteran'], { replaceUrl: true });
       })
       .catch((err) => {
-        console.log("eroororoo");
-        
-        console.log(err,"hhghg");
+        console.log(err);
       });
   }
 
-  ngOnInit(): void {
-    this.buildForm();
-    console.log(window.location.href);
-  }
-
-  buildForm() {
-    this.loginForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-    });
-  }
-
-  get username() {
-    return this.loginForm.get('username');
-  }
-  get password() {
-    return this.loginForm.get('password');
-  }
+  ngOnInit(): void {}
 
   navigateToDashboard(): void {
-   window.location.assign(environment.loginURL);
-    // Auth.federatedSignIn();
-    
+    Auth.federatedSignIn();
   }
 }
