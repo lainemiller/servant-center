4d4ed2c3378e417e5b9890936b7514bef48fcf87
Akash kumar <akash.kumar6@mindtree.com>
2021-12-13 11:29:45 +0530

    consent form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     path: 'veteran',
     loadChildren: () =>
       import('./veteran/veteran.module').then((m) => m.VeteranModule),
-      canActivate: [AuthGuard]
+      canActivate: [VeteranGuard]
   },
   { path: '**', redirectTo: 'veteran' },
 ];
