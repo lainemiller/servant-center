73bc68c0baf3af21751abe2f739c888e9fbd9d03
Pravin Bhilare <pravin.bhilare@mindtree.com>
2022-08-08 17:52:31 +0530

    updated changes after lint

--- src/app/veteran/components/veteran-profile/veteran-profile.component.ts
+++ src/app/veteran/components/veteran-profile/veteran-profile.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './veteran-profile.component.html',
   styleUrls: ['./veteran-profile.component.scss'],
 })
-export class VeteranProfileComponent implements OnInit {
+export class VeteranProfileComponent {
   // to show few fields in veteran profile
   isShowFields: boolean = true;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
     consentUpdateUser: 'https://h0p82a84v8.execute-api.us-east-1.amazonaws.com/test_v1/consentForm/acceptConsent/',
     veteranProfileGetUser:'https://h0p82a84v8.execute-api.us-east-1.amazonaws.com/test_v1/userProfile/getUserDetails/',
     caseWorkerUser:'https://h0p82a84v8.execute-api.us-east-1.amazonaws.com/test_v1/uiLayout/getCaseWorkerDetails/3'
-  }
+  },
+  localUrl:''
 };
