2a37af2f326087f5b01bc56faf765a27e409e002
Sam Kamalesan <sam.kamalesan@lfg.com>
2022-09-06 13:06:53 +0530

    modify aws amplify configuration

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,24 +1,29 @@
-const awsConfig = {
+import awsConfig from "./aws-configuration";
+
+const winHostNm = window.location.hostname;
+let envConfig: any;
+
+if (winHostNm === 'localhost' || winHostNm.includes('dev') || winHostNm.includes('dd1w1wuqwe28t')) {
+  envConfig = awsConfig.oauth.dev;
+} else {
+  envConfig = awsConfig.oauth.prod;
+}
+
+console.log('aws exports::envConfig', envConfig, winHostNm);
+
+const awsAmplifyConfig = {
   aws_project_region: 'us-east-1',
   aws_cognito_region: 'us-east-1',
   aws_user_pools_id: 'us-east-1_SpellypxJ',
   aws_user_pools_web_client_id: '3ebsuhsnrt5j4n2hsko4n0m88r',
   oauth: {
-    dev: {
-      domain: 'servant-center-dev.auth.us-east-1.amazoncognito.com',
-      scope: ['email', 'openid'],
-      redirectSignIn: 'https://dd1w1wuqwe28t.cloudfront.net/veteran',
-      redirectSignOut: 'https://dd1w1wuqwe28t.cloudfront.net/login',
-      responseType: 'code',
-    },
-    prod: {
-      domain: 'servant-center.auth.us-east-1.amazoncognito.com',
-      scope: ['email', 'openid'],
-      redirectSignIn: 'https://dd1w1wuqwe28t.cloudfront.net/veteran',
-      redirectSignOut: 'https://dd1w1wuqwe28t.cloudfront.net/login',
-      responseType: 'code',
-    }
+    domain: envConfig.domain,
+    scope: ['email', 'openid'],
+    redirectSignIn: envConfig.redirectSignIn,
+    redirectSignOut: envConfig.redirectSignOut,
+    responseType: 'code',
+    redirect_uri: envConfig.redirectSignIn
   },
 };
 
-export default awsConfig;
+export default awsAmplifyConfig;
