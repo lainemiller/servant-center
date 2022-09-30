20dc64375e7350dfd3fcf6a0c085177e117b7d32
Siddanagowda C K <Siddanagowda.K@mindtree.com>
2022-09-30 09:11:22 +0530

    post API for ia page 3 is working

--- src/app/case-worker/components/resident-search/rs-initial-assessment/ia-form-page-three/ia-form-page-three.component.ts
+++ src/app/case-worker/components/resident-search/rs-initial-assessment/ia-form-page-three/ia-form-page-three.component.ts
@@ -2,17 +2,21 @@ import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IaPage3Service } from 'src/app/case-worker/services/ia-page3.service';
-
+import { ClipBoardService } from 'src/app/shared/services/clip-board.service';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-ia-form-page-three',
   templateUrl: './ia-form-page-three.component.html',
   styleUrls: ['./ia-form-page-three.component.scss'],
+  providers: [MessageService],
 })
 export class IaFormPageThreeComponent implements OnInit {
   page3Form!: FormGroup;
-  // ia3: boolean = true;
-  // greyingOut: boolean = true;
+  ia3: boolean = true;
+  greyingOut: boolean = true;
+  data: any;
   submitted!: boolean;
+  selecteVetId: any;
   mentalStatusAssessment!: FormGroup;
   medicalInformation!: FormGroup;
   ideation!: FormGroup;
@@ -25,7 +29,7 @@ export class IaFormPageThreeComponent implements OnInit {
     { label: 'Slim', value: 'slim' },
     { label: 'Obese', value: 'obese' },
   ];
-selectedgeneralAppearance: any []= [];
+  selectedgeneralAppearance: any[] = [];
   thoughtForumList: any = [
     { label: 'Logical', value: 'logical' },
     { label: 'Coherent', value: 'coherent' },
@@ -36,7 +40,7 @@ selectedgeneralAppearance: any []= [];
     { label: 'Flight of Ideas', value: 'flight of ideas' },
     { label: 'Thought blocking', value: 'Thought blocking' },
   ];
-selectdeThoughtForum: any []= [];
+  selectdeThoughtForum: any[] = [];
   moodList = [
     { label: 'Depressed', value: 'depressed' },
     { label: 'Elated', value: 'elated' },
@@ -44,8 +48,8 @@ selectdeThoughtForum: any []= [];
     { label: 'Happy', value: 'happy' },
     { label: 'Neutral', value: 'neutral' },
   ];
-answeredByClient: any [] = [];
-observedByInterviewer: any []= [];
+  answeredByClient: any[] = [];
+  observedByInterviewer: any[] = [];
   affectList = [
     { label: 'Appropriate', value: 'appropriate' },
     { label: 'Fearful', value: 'fearful' },
@@ -55,26 +59,112 @@ observedByInterviewer: any []= [];
     { label: 'Liable', value: 'liable' },
     { label: 'Inappropriate', value: 'inappropriate' },
   ];
-  selectedAffect: any [] = [];
+  selectedAffect: any[] = [];
 
   ideationList = [
-    { label: 'Thoughts of Suicide', key: 'thoughtsOfSuicide' },
-    { label: 'Plan', key: 'suicidePlan' },
-    { label: 'Thoughts of Homicide', key: 'thoughtsOfHomicide' },
-    { label: 'Plan', key: 'homicidePlan' },
-    { label: 'Delusional', key: 'delusional' },
-    { label: 'Paranoid', key: 'paranoid' },
-    { label: 'Hallucinations', key: 'hallucinations' },
+    { label: 'Thoughts of Suicide', key: 'thoughtsOfSuicide', value: '' },
+    { label: 'Sucide Plan', key: 'suicidePlan', value: '' },
+    { label: 'Thoughts of Homicide', key: 'thoughtsOfHomicide', value: '' },
+    { label: 'Homicide Plan', key: 'homicidePlan', value: '' },
+    { label: 'Delusional', key: 'delusional', value: '' },
+    { label: 'Paranoid', key: 'paranoid', value: '' },
+    { label: 'Hallucinations', key: 'hallucinations', value: '' },
   ];
 
-  memoryList = [
-    { label: 'Recent Memory', key: 'recentMemory' },
-    { label: 'Remote Memory', key: 'remoteMemory' },
+  recentMemory = [
+    { label: 'Yes', value: true },
+    { label: 'No', value: false },
   ];
-  constructor(private fb: FormBuilder, private router: Router, private service: IaPage3Service) {}
+  remoteMemory = [
+    { label: 'Yes', value: true },
+    { label: 'No', value: false },
+  ];
+
+  underSpecialistCare = [
+    { label: 'Yes', value: true },
+    { label: 'No', value: false },
+  ];
+
+  priPhyscianPartOfGP = [
+    { label: 'Yes', value: true },
+    { label: 'No', value: false }
+  ];
+  constructor(private fb: FormBuilder, private router: Router,
+    private messageService: MessageService,
+    private service: IaPage3Service,
+    private cacheData: ClipBoardService) {
+    this.selecteVetId = this.cacheData.get('selectedResidentVeteranId');
+    console.log('sel', this.selecteVetId);
+    this.setForm();
+  }
 
   ngOnInit(): void {
     this.initializeFormGroups();
+  }
+
+  setForm() {
+    this.service.getIAPage3(this.selecteVetId).subscribe((res) => {
+      this.ia3 = false;
+      this.greyingOut = false;
+      this.data = res[0];
+      this.buildForm();
+      if (this.data) {
+        this.mentalStatusAssessment.patchValue({
+          date: this.data.orientation_date,
+          time: this.data.orientation_time,
+          person: this.data.orientation_person,
+          generalAppearance: this.data.general_appearance[0] ,
+          thoughtsOfSuicide: this.data.ideation,
+          place: this.data.orientation_place,
+          affect: this.data.affect,
+          recentMemory: this.data.intact_recent_memory,
+          remoteMemory: this.data.intact_remote_memory
+        });
+
+        this.medicalInformation.patchValue({
+          primaryPhysicianName: this.data.primary_physician,
+          phone: this.data.primary_physician_phone,
+          clinic: this.data.clinic,
+          hospital: this.data.hospital,
+          diagnosis: this.data.diagnosis,
+          currentMedication: this.data.current_medications,
+          primaryCareProvider: this.data.primary_care_provider,
+          priPhyscianPartOfGP: this.data.primary_physician_part_of_gp,
+          physcianGP: this.data.physician_gp,
+          clinicLocation: this.data.clinic_location,
+          hospitalLocation: this.data.hospital_location,
+          underSpecialistCare: this.data.under_specialist_care,
+          specialistType: this.data.specialist_type,
+          specialistName: this.data.specialist_name,
+          currentTreatment: this.data.current_treatment
+        });
+      }
+      else {
+
+        this.mentalStatusAssessment.patchValue({
+          generalAppearance: null,
+          thoughtsOfSuicide: null,
+
+        });
+        this.medicalInformation.patchValue({
+          primaryPhysicianName: null,
+          phone: null,
+          clinic: null,
+          hospital: null,
+          diagnosis: null,
+          currentMedication: null,
+          primaryCareProvider: null,
+          priPhyscianPartOfGP: null,
+          physcianGP: null,
+          clinicLocation: null,
+          hospitalLocation: null,
+          underSpecialistCare: null,
+          specialistType: null,
+          specialistName: null,
+          currentTreatment: null
+        });
+      }
+    })
   }
 
   initializeFormGroups() {
@@ -89,35 +179,40 @@ observedByInterviewer: any []= [];
     });
 
     this.mentalStatusAssessment = this.fb.group({
-      orientation: this.fb.group({
-        date: [''],
-        time: [''],
-        place: [''],
-        person: [''],
-      }),
+      veteranId: [this.selecteVetId],
+      date: [''],
+      time: [''],
+      place: [''],
+      person: [''],
       generalAppearance: [this.selectedgeneralAppearance, Validators.required],
       thoughtForum: [this.selectdeThoughtForum, Validators.required],
-      mood: this.fb.group({
         answeredByClient: [this.answeredByClient, Validators.required],
         observedByInterviewer: [this.observedByInterviewer, Validators.required],
-      }),
-      affect:[this.selectedAffect, Validators.required],
+      affect: [this.selectedAffect, Validators.required],
       ideation: this.ideation,
-      memory: this.fb.group({
         recentMemory: [],
         remoteMemory: [],
-      }),
+        recentMemoComments: [],
+        remoteMemoComments: [],
     });
     this.medicalInformation = this.fb.group({
-      primaryPhysicianName: [],
-      phone: [],
-      clinic: [],
-      hospital: [],
-      physicianSpecialist: [],
-      diagnosisAndCurrentTreatment: [],
-      currentMedication: [],
+      primaryPhysicianName: ['', Validators.required],
+      phone: ['', Validators.required],
+      clinic: ['', Validators.required],
+      hospital: ['', Validators.required],
+      diagnosis: ['', Validators.required],
+      currentMedication: ['', Validators.required],
+      primaryCareProvider: ['', Validators.required],
+      priPhyscianPartOfGP: ['', Validators.required],
+      physcianGP: ['', Validators.required],
+      clinicLocation: ['', Validators.required],
+      hospitalLocation: ['', Validators.required],
+      underSpecialistCare: ['', Validators.required],
+      specialistType: ['', Validators.required],
+      specialistName: ['', Validators.required],
+      currentTreatment: ['', Validators.required],
+      veteranId: [this.selecteVetId]
     });
-    this.buildForm();
   }
 
   buildForm() {
@@ -130,33 +225,40 @@ observedByInterviewer: any []= [];
 
   onSubmit() {
     this.submitted = true;
+    let specialistType = this.medicalInformation.value.specialistType;
+    let currMedications = this.medicalInformation.value.currentMedication;
+    this.medicalInformation.value.specialistType = "{" + specialistType + "}";
+    this.medicalInformation.value.currentMedication = "{" + currMedications + "}";
     this.service
       .initialTreatmentGoalsPage3(this.page3Form.value)
       .subscribe((data) => {
         console.log('Submitted');
       });
-      // this.router.navigateByUrl(
-      //   'case-worker/resident-search/initial-assessment/page-4'
-      // );
+    // this.router.navigateByUrl(
+    //   'case-worker/resident-search/initial-assessment/page-4'
+    // );
     console.log('page 3 values', this.page3Form.value);
   }
 
-  next(){
+  next() {
     console.log('clicked next');
-    if(this.submitted){
-    this.router.navigateByUrl(
-      'case-worker/resident-search/initial-assessment/page-4'
-    );
+    if (this.submitted) {
+      this.router.navigateByUrl(
+        'case-worker/resident-search/initial-assessment/page-4'
+      );
     }
-    else{
+    else {
       alert("Please save first")
     }
   }
+  reset() {
 
-  goBack(){
+  }
+
+  goBack() {
     this.router.navigateByUrl(
       'case-worker/resident-search/initial-assessment/page-2'
-    );  
+    );
   }
 
   get ideationControls() {
