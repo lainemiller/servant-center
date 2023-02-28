c5c74518acf62335edeab560269359cd9040c208
Yuvaganesh17 <Yuvaganesh.S@mindtree.com>
2023-02-28 14:51:30 +0530

    File Upload commit

--- src/app/case-worker/services/resident-search.service.ts
+++ src/app/case-worker/services/resident-search.service.ts
@@ -1,52 +1,60 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable,isDevMode } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RestClientService } from 'src/app/shared/services/rest-client.service';
 import { environment } from 'src/environments/environment';
-import { environment as env} from 'src/environments/environment.prod';
+import { environment as env } from 'src/environments/environment.prod';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ResidentSearchService {
   private isDev = isDevMode();
-  constructor(private http: HttpClient, private restcs:RestClientService) {}
-  private commonUrl=environment.localUrl;
+  constructor(private http: HttpClient, private restcs: RestClientService) {}
+  private commonUrl = environment.localUrl;
   private getCwNicknameDetails = env.serviceUrl.getCwNicknameDetails;
   private getCwUserNameDetails = env.serviceUrl.getCwUserNameDetails;
   private postAddNewVeteran = env.serviceUrl.addNewVeteranRS;
-  private residentSearchAPI= env.serviceUrl.getResidentSearchData;
+  private residentSearchAPI = env.serviceUrl.getResidentSearchData;
+  private uploadFileAPI = env.serviceUrl.uploadMiscFile;
 
   getResidentSearchData() {
-    if(this.isDev){
-    return this.restcs.get(this.commonUrl+'residentSearch/getAll');
+    if (this.isDev) {
+      return this.restcs.get(this.commonUrl + 'residentSearch/getAll');
     } else {
       return this.restcs.get(this.residentSearchAPI);
     }
-
   }
 
-  public getCwNickName(): Observable<any>{
-    if(this.isDev){
-    return this.restcs.get(this.commonUrl+ 'getCaseWorkerNickname');
-  } else {
-    return this.restcs.get(this.getCwNicknameDetails);
+  public getCwNickName(): Observable<any> {
+    if (this.isDev) {
+      return this.restcs.get(this.commonUrl + 'getCaseWorkerNickname');
+    } else {
+      return this.restcs.get(this.getCwNicknameDetails);
+    }
   }
-}
 
-public getCwUsername(): Observable<any>{
-   if(this.isDev){
-    return this.restcs.get(this.commonUrl+ 'getWebpartyUsername');
-   } else {
-    return this.restcs.get(this.getCwUserNameDetails);
+  public getCwUsername(): Observable<any> {
+    if (this.isDev) {
+      return this.restcs.get(this.commonUrl + 'getWebpartyUsername');
+    } else {
+      return this.restcs.get(this.getCwUserNameDetails);
+    }
   }
-}
 
-public addnewVeteran(data: any): Observable<any> {
-  if (this.isDev) {
-    return this.restcs.post(this.commonUrl + 'addNewVeteran/', data);
-  } else {
-    return this.restcs.post(this.postAddNewVeteran, data);
+  public addnewVeteran(data: any): Observable<any> {
+    if (this.isDev) {
+      return this.restcs.post(this.commonUrl + 'addNewVeteran/', data);
+    } else {
+      return this.restcs.post(this.postAddNewVeteran, data);
+    }
   }
-}
+
+  public uploadMiscFile(file: FormData): Observable<any> {
+    if (this.isDev) {
+      return this.http.post(this.commonUrl + 'fileUpload', file);
+    } else {
+      return this.http.post(this.uploadFileAPI, file);
+    }
+  }
 }
